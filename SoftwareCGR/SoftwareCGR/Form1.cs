deb2e4bf54597b20cb531e9e2ff3eaa7c691e042
cgonzalez7685 <gonzalezcarlos7684@outlook.com>
2018-02-20 12:11:31 -0600

    probandoo

--- SoftwareCGR/SoftwareCGR/Form1.cs
+++ SoftwareCGR/SoftwareCGR/Form1.cs
@@ -16,5 +16,10 @@ namespace SoftwareCGR
         {
             InitializeComponent();
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int y = 100;
+        }
     }
 }
